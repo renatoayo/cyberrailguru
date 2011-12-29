be7d234caa8355fb5532b6f5a8f568e53c581180
cyberreefguru <cyberreefguru@gmail.com>
2011-12-29 21:40:24 +0000

    Initial check in

---
+++ arduino/SoftwareSerial/trunk/Debug/IoShield.d
@@ -0,0 +1,27 @@
+IoShield.d IoShield.o: ../IoShield.cpp ../IoShield.h \
+  /Users/tom/Development/arduino/avrlib/WProgram.h \
+  /Users/tom/Development/arduino/avrlib/wiring.h \
+  /Users/tom/Development/arduino/avrlib/binary.h \
+  /Users/tom/Development/arduino/avrlib/WCharacter.h \
+  /Users/tom/Development/arduino/avrlib/WString.h \
+  /Users/tom/Development/arduino/avrlib/HardwareSerial.h \
+  /Users/tom/Development/arduino/avrlib/Stream.h \
+  /Users/tom/Development/arduino/avrlib/Print.h
+
+../IoShield.h:
+
+/Users/tom/Development/arduino/avrlib/WProgram.h:
+
+/Users/tom/Development/arduino/avrlib/wiring.h:
+
+/Users/tom/Development/arduino/avrlib/binary.h:
+
+/Users/tom/Development/arduino/avrlib/WCharacter.h:
+
+/Users/tom/Development/arduino/avrlib/WString.h:
+
+/Users/tom/Development/arduino/avrlib/HardwareSerial.h:
+
+/Users/tom/Development/arduino/avrlib/Stream.h:
+
+/Users/tom/Development/arduino/avrlib/Print.h:
