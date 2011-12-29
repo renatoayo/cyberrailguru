6bd934ede4ec2e735ea5a344e5b422049ed7246a
cyberreefguru <cyberreefguru@gmail.com>
2011-12-29 21:51:11 +0000

    Initial check in

---
+++ arduino/IoShieldLib/trunk/Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../IoShield.cpp 
+
+CPP_DEPS += \
+./IoShield.d 
+
+OBJS += \
+./IoShield.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR C++ Compiler'
+	avr-g++ -I"/Users/tom/Development/arduino/avrlib" -I/usr/local/CrossPack-AVR-20100115/include -I/usr/local/CrossPack-AVR-20100115/avr-4/include -Wall -Os -fpack-struct -fshort-enums -std=gnu++98 -funsigned-char -funsigned-bitfields -fno-exceptions -mmcu=atmega328p -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
