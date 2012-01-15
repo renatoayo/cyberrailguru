86574429ad029bc1a9e3a8dd039cbf6ae5edbf7a
cyberreefguru <cyberreefguru@gmail.com>
2012-01-15 02:14:28 +0000

    Updated software to align with PCB. Added  4 LEDs to indicate motor movement

--- arduino/TopSpinApp/trunk/TopSpin.h
+++ arduino/TopSpinApp/trunk/TopSpin.h
@@ -14,26 +14,54 @@
 #include <avr/pgmspace.h>
 #include <FlexiTimer2.h>
 
+//PORTD maps to Arduino digital pins 0 to 7
+//    DDRD - The Port D Data Direction Register - read/write
+//    PORTD - The Port D Data Register - read/write
+//    PIND - The Port D Input Pins Register - read only
+//
+//PORTB maps to Arduino digital pins 8 to 13 The two high bits (6 & 7) map to the crystal pins and are not usable
+//    DDRB - The Port B Data Direction Register - read/write
+//    PORTB - The Port B Data Register - read/write
+//    PINB - The Port B Input Pins Register - read only
+//
+//PORTC maps to Arduino analog pins 0 to 5. Pins 6 & 7 are only accessible on the Arduino Mini
+//    DDRC - The Port C Data Direction Register - read/write
+//    PORTC - The Port C Data Register - read/write
+//    PINC - The Port C Input Pins Register - read only
+
 
 #define LED_PIN		13
 
-#define MAIN_ARM	0
-#define CHAIR		1
+#define MAIN_ARM	0 // LEFT
+#define CHAIR		1 // RIGHT
 
 #define BASE_INPUT_PIN	11
 #define SIDE_INPUT_PIN	12
 
-#define BASE_STATUS_PIN	6
-#define SIDE_STATUS_PIN	7
+//#define BASE_STATUS_PIN	6
+//#define SIDE_STATUS_PIN	7
 
 #define FORWARD 	0
 #define BACKWARD 	1
+#define STOP 		2
 
-// Right Motor -> Y1 = Blue
-// Right Motor -> Y2 = Red/Orange
+// Base -> In2
+// Side -> In3
+// Right Motor -> 1Y -> O1.1 -> RED
+// Right Motor -> 2Y -> O1.2 -> BLUE
+// Left Motor  -> 3Y -> O2.1 -> RED
+// Left Motor  -> 4Y -> O2.2 -> BLUE
 
+
+#define NUM_MOTORS	2
 uint8_t motor[2][3] = { {2, 3, 9}, {4, 5, 10} }; // Input 1, Input 2, Enable
 
+#define NUM_LEDS	4
+#define ON 			true
+#define OFF			false
+#define DONT_CARE	2
+
+uint8_t leds[4] = {0,1,2,3}; // Analog port (Port C)
 
 
 
