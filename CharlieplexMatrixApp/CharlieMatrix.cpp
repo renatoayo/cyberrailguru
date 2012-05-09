e68b0f8f25831a93535d874e9c99d8b846f6a414
cyberreefguru@gmail.com <cyberreefguru@gmail.com@7bf822b0-cd2a-c3ae-bf5e-27838ea3eb47>
2012-05-09 00:32:26 +0000

    initial check in

---
+++ CharlieplexMatrixApp/CharlieMatrix.cpp
@@ -0,0 +1,228 @@
+/*
+ * CharlieMatrix.cpp
+ *
+ *  Created on: May 8, 2012
+ *      Author: tom
+ */
+
+#include "CharlieMatrix.h"
+
+
+///** Define the static variable for Charlieplex */
+CharlieMatrix CharlieMatrixPort;
+
+
+static uint8_t enableOutput;
+uint8_t patternBuffer[NUMBER_OF_ROWS];
+static uint8_t portPatternBuffer[2][NUMBER_OF_ROWS];
+static uint8_t portDirectionBuffer[2][NUMBER_OF_ROWS];
+static uint8_t currentRowIndex;
+static uint8_t currentBufferIndex;
+
+
+
+/**
+ * Constructor
+ */
+CharlieMatrix::CharlieMatrix()
+{
+}
+
+
+/**
+ * Initializes the library to use a specific port and pin mapping
+ */
+void CharlieMatrix::initialize()
+{
+
+	uint8_t i;
+
+#ifdef __DEBUG
+	// Initialize hardware serial port
+	Serial.begin(115200);
+	Serial.println("INITIALIZING");
+#endif
+
+	 // Set pin 13 to output
+	pinMode( LED_PIN, OUTPUT );
+	digitalWrite(LED_PIN, LOW);
+
+	clear();
+
+	// Initialize variables
+	enableOutput = false;
+	currentBufferIndex = 1;
+	currentRowIndex = 0;
+
+	for(i=0; i<NUMBER_OF_ROWS; i++)
+	{
+		patternBuffer[i] = 0x00;
+		portPatternBuffer[0][i] = 0x00;
+		portPatternBuffer[1][i] = 0x00;
+		portDirectionBuffer[0][i] = 0x00;
+		portDirectionBuffer[1][i] = 0x00;
+	}
+
+	// Initialize timer (500us interval)
+	FlexiTimer2::set(1, CharlieMatrix::driveRow );
+
+	// Start the timer
+	FlexiTimer2::start();
+
+#ifdef __DEBUG
+	Serial.println("INITIALIZED");
+#endif
+
+}
+
+/**
+ * Returns the address of the pattern buffer
+ *
+ */
+uint8_t* CharlieMatrix::getPatternBuffer()
+{
+	return patternBuffer;
+}
+
+
+/**
+ * Enables or disables display of the pattern
+ */
+void CharlieMatrix::enable(uint8_t flag)
+{
+	if( flag == false)
+	{
+		clear();
+	}
+	enableOutput = flag;
+}
+
+/**
+ * Reads patternBuffer, computes drivable pattern,
+ * computes port mask, and switches buffer.
+ */
+void CharlieMatrix::setPattern()
+{
+	uint8_t i;
+	uint8_t bufferIndex;
+
+	// Compute next buffer - if current=0, 1; else 1
+	bufferIndex = (currentBufferIndex+1) & 0x01;
+
+	// Compute patterns to drive
+	for(i=0; i<NUMBER_OF_ROWS; i++)
+	{
+		// Computer HW agnostic pattern
+		portPatternBuffer[bufferIndex][i] = convertRow( i, patternBuffer[i]);
+		portDirectionBuffer[bufferIndex][i] = portPatternBuffer[bufferIndex][i] | (0x01 << i); // all the ones and the row
+
+#ifdef __DEBUG
+	Serial.print("ROW");
+	Serial.print( i, HEX );
+	Serial.print(": PATTERN: ");
+	Serial.print( patternBuffer[i], HEX );
+	Serial.print(" PORT: ");
+	Serial.print( portPatternBuffer[bufferIndex][i], HEX );
+	Serial.print(" DIR: ");
+	Serial.println( portDirectionBuffer[bufferIndex][i], HEX );
+#endif
+
+	} // end for
+
+	// Switch drive buffer
+	currentBufferIndex = bufferIndex;
+
+} // end setPattern
+
+
+/**
+ * Clears the output port
+ *
+ * NOTE: This routine is specific to the HW
+ */
+void CharlieMatrix::clear()
+{
+	// Set pins as inputs
+	PORTD &= 0x03; // Set all to low
+	DDRD &= 0x03;
+}
+
+
+/**
+ * Drives the pattern onto the port
+ *
+ * NOTE: This routine is specific to the hardware
+ */
+void CharlieMatrix::driveRow()
+{
+	if( enableOutput == true )
+	{
+		// Clear direction
+		PORTD &= 0x03; // Set all to low
+		DDRD &= 0x03; // clear direction
+		DDRD = DDRD | (portDirectionBuffer[currentBufferIndex][currentRowIndex] << 2);
+		PORTD = PORTD | (portPatternBuffer[currentBufferIndex][currentRowIndex] << 2);
+
+		currentRowIndex++;
+		if( currentRowIndex >= NUMBER_OF_ROWS )
+		{
+			currentRowIndex = 0;
+		}
+	}
+}
+
+/**
+ * Converts a straight 8-bit pattern into a "driveable" pattern
+ * according to abstract schematic
+ *
+ * returns pattern to be driven
+ */
+uint8_t CharlieMatrix::convertRow(uint8_t row, uint8_t rowPattern)
+{
+	uint8_t pattern;
+
+	switch( row )
+	{
+		case 0:
+			pattern = rowPattern << 1;
+			break;
+		case 1:
+			pattern = (rowPattern & 0x01) | ((rowPattern & 0xfe) << 1);
+			break;
+		case 2:
+			pattern = (rowPattern & 0x03) | ((rowPattern & 0xfc) << 1);
+			break;
+		case 3:
+			pattern = (rowPattern & 0x07) | ((rowPattern & 0xf8) << 1);
+			break;
+		case 4:
+			pattern = (rowPattern & 0x0f) | ((rowPattern & 0xf0) << 1);
+			break;
+		case 5:
+			pattern = rowPattern & 0x1f;
+			break;
+		default:
+			pattern = 0x00;
+			break;
+	}
+
+#ifdef __DEBUG
+	Serial.print("PATTERN: ");
+	Serial.println( pattern, HEX );
+#endif
+
+	return pattern;
+}
+
+void CharlieMatrix::clearBuffer()
+{
+	uint8_t i;
+
+	for(i=0; i<NUMBER_OF_ROWS; i++)
+	{
+		patternBuffer[i] = 0x00;
+	}
+
+}
+
+
---
+++ CharlieplexMatrixApp/main.c
@@ -0,0 +1,275 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 26, 2011
+ *      Author: Thomas M. Sasala
+ *
+ *  Controller for Faller Top Spin
+ *
+ * Copyright (c) 2011 Thomas M. Sasala.  All right reserved.
+ *
+ * This work is licensed under the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View,
+ * California, 94041, USA.
+ *
+ */
+
+#include "CharlieMatrix.h"
+
+
+
+
+/**
+ * Main application
+ *
+ */
+int main(void)
+{
+	uint8_t i, j, temp;
+//	uint16_t temp;
+//	uint8_t row, col, data;
+
+	uint8_t *buf;
+
+	// Initialize libraries
+	init();
+
+	CharlieMatrixPort.initialize();
+
+
+
+//	// Initialize variables
+//	currentBufferIndex = 1;
+//	currentRowIndex = 0;
+//
+//	for(i=0; i<NUMBER_OF_ROWS; i++)
+//	{
+//		patternBuffer[i] = 0x00;
+//		portPatternBuffer[0][i] = 0x00;
+//		portPatternBuffer[1][i] = 0x00;
+//		portDirectionBuffer[0][i] = 0x00;
+//		portDirectionBuffer[1][i] = 0x00;
+//	}
+
+	buf = CharlieMatrixPort.getPatternBuffer();
+
+//	buf[0] = B00000001;
+//	buf[1] = B00000010;
+//	buf[2] = B00000100;
+//	buf[3] = B00001000;
+//	buf[4] = B00010000;
+//	buf[5] = B00100000;
+
+	buf[0] = B00000000;
+	buf[1] = B00000000;
+	buf[2] = B00000000;
+	buf[3] = B00000000;
+	buf[4] = B00000000;
+	buf[5] = B00000000;
+
+
+	CharlieMatrixPort.setPattern();
+
+	CharlieMatrixPort.enable(true);
+
+//	while(1);
+
+	temp = 0x01;
+
+	for(j=0; j<5; j++)
+	{
+		for(i=0; i<6; i++)
+		{
+			buf[i] = temp;
+		}
+		CharlieMatrixPort.setPattern();
+		delay(100);
+		temp = temp << 1;
+	}
+
+	CharlieMatrixPort.clear();
+	CharlieMatrixPort.clearBuffer();
+
+	for(i=0; i<6; i++)
+	{
+		buf[i] = 0xff;
+		CharlieMatrixPort.setPattern();
+		delay(100);
+	}
+
+	CharlieMatrixPort.clear();
+	CharlieMatrixPort.clearBuffer();
+
+	temp = 0x00;
+	for(j=0; j<6; j++)
+	{
+		for(i=0; i<5; i++)
+		{
+			temp = ((temp << 1) | 0x01);
+			buf[j] = temp;
+			CharlieMatrixPort.setPattern();
+			delay(100);
+		}
+		temp = 0x00;
+	}
+
+
+
+//	drivePattern();
+//
+//	while(1)
+//	{
+//		for(i=0; i<NUMBER_OF_ROWS; i++)
+//		{
+//			currentRowIndex = i;
+//			driveRow( );
+//			delay(1);
+////			if( j == 1 )
+////			{
+////				Serial.print("ROW: ");
+////				Serial.print( i, HEX );
+////				Serial.print( " PATTERN: ");
+////				Serial.println( data, HEX );
+////			}
+//		}
+//		j = 0;
+//
+//	}
+
+
+#ifdef __DEBUG
+	Serial.println("PROGRAM END");
+#endif
+
+	// Hold forever
+	while(1);
+
+} // end main
+
+
+
+
+//void pattern(uint16_t led)
+//{
+//	CharliePort.setPattern( led );
+//}
+//
+//
+//void set(uint8_t led)
+//{
+//	CharliePort.setPattern( CharliePort.getPattern() | (1<< (led-1)) );
+//}
+//
+//void unset(uint8_t led)
+//{
+//	CharliePort.setPattern( CharliePort.getPattern() & ~(1<< (led-1)) );
+//}
+//
+//
+//void roll(uint16_t pattern, uint8_t direction, uint16_t wait, uint8_t repeat)
+//{
+//	uint16_t i, temp;
+//
+//	CharliePort.setPattern(pattern);
+//	for(i=0; i<repeat; i++)
+//	{
+//		delay(wait);
+//		if( direction == LEFT )
+//		{
+//			temp = CharliePort.getPattern() << 1;
+//			if( (temp & 0x1000) == 0x1000)
+//			{
+//				temp |= 0x0001;
+//			}
+//			CharliePort.setPattern(temp);
+//		}
+//		else
+//		{
+//			temp = CharliePort.getPattern() >> 1;
+//			if( (CharliePort.getPattern() & 0x0001) == 0x01)
+//			{
+//				temp |= 0x0800;
+//			}
+//			CharliePort.setPattern(temp);
+//		}
+//	}
+//}
+//
+//void rotate(uint16_t pattern, uint8_t direction, uint16_t wait, uint8_t repeat)
+//{
+//	uint16_t i;
+//	CharliePort.setPattern(pattern);
+//	for( i=0; i<repeat; i++)
+//	{
+//		if( direction == LEFT )
+//		{
+//			CharliePort.setPattern( CharliePort.getPattern() << 1 );
+//		}
+//		else
+//		{
+//			CharliePort.setPattern( CharliePort.getPattern() >> 1 );
+//		}
+//		delay(wait);
+//	}
+//}
+//
+//
+//void playHeartbeat(uint16_t pattern, uint16_t repeat, uint16_t hbOff, uint16_t hbOn, uint16_t hbPause)
+//{
+//	uint16_t i;
+//
+//	if( repeat > 0 )
+//	{
+//		for(i=0; i<repeat; i++)
+//		{
+//			CharliePort.setPattern(pattern);
+//			delay( hbOn );
+//			CharliePort.setPattern(0x00);
+//			delay(hbPause);
+//			CharliePort.setPattern(pattern);
+//			delay( hbOn );
+//			CharliePort.setPattern( 0x00 );
+//			delay( hbOff );
+//		}
+//	}
+//}
+//
+//void stack(uint8_t direction, uint16_t wait, uint8_t numLeds)
+//{
+//	uint16_t i, j, k, temp, pattern;
+//
+//	pattern = 0x0000;
+//	j = numLeds;
+//
+//	for(i=0; i<numLeds; i++)
+//	{
+//		temp = 0x0001;
+//		for(k=0; k<j; k++)
+//		{
+//			if( direction == RIGHT )
+//			{
+//				temp = 0x01 << ( (numLeds-1) - k);
+//			}
+//			else
+//			{
+//				temp = 0x01 << k;
+//			}
+//
+//			CharliePort.setPattern( (temp | pattern) );
+//			delay( wait );
+//		}
+//		pattern |= temp;
+//		j--;
+////		delay( wait );
+//	}
+//}
+
+extern "C" void __cxa_pure_virtual()
+{
+	cli();
+	for (;;);
+}
+
