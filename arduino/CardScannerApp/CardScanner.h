0557d9f0da48a054cdd129bd0469f588eec4a15d
cyberreefguru <cyberreefguru@gmail.com>
2012-12-24 22:28:45 +0000

    Initial import.

---
+++ arduino/CardScannerApp/CardScanner.h
@@ -0,0 +1,30 @@
+/*
+ * CardScanner.h
+ *
+ *  Created on: Jul 15, 2011
+ *      Author: tom
+ *
+ * Copyright (c) 2011 Thomas M. Sasala.  All right reserved.
+ *
+ * This work is licensed under the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ * To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View,
+ * California, 94041, USA.
+ */
+
+#ifndef CARD_SCANNER_H_
+#define CARD_SCANNER_H_
+
+#include <WProgram.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include "Lcd.h"
+#include "RealTimeClock.h"
+
+
+#endif /* CARD_SCANNER_H_ */
