7b3c2d08853aefaf5a8997f1c99ab28b634e065d
cyberreefguru@gmail.com <cyberreefguru@gmail.com@7bf822b0-cd2a-c3ae-bf5e-27838ea3eb47>
2013-10-13 19:28:50 +0000

    initial import

--- arduino/AqMonitorApp/trunk/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-LIBS := -lWireLib -lSoftwareSerialLib -lDiecimilaCore
-
-USER_OBJS :=
-
---
+++ arduino/LedFlasher.h
@@ -0,0 +1,32 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef LedFlasher_H_
+#define LedFlasher_H_
+
+#include "Arduino.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project LedFlasher here
+
+
+
+
+//Do not add code below this line
+#endif /* LedFlasher_H_ */
