618e9e12deaf8aa1ad467d9d84e58dfc55adb38d
cyberreefguru <cyberreefguru@gmail.com>
2014-02-02 15:10:08 +0000

    moved to macros for port expansion and greatly simplified the code.

--- arduino/PowerTowerLights/PowerTowerLights.h
+++ arduino/PowerTowerLights/PowerTowerLights.h
@@ -8,28 +8,10 @@
 #define PowerTowerLights_H_
 
 #include <Arduino.h>
-#include "LedShieldDriver.h"
+//#include "LedShieldDriver.h"
 #include "LedShieldDriver2.h"
 #include "FlexiTimer2.h"
 
-// ROW_CLK D4, ROW_DATA D5, ROW_LATCH D6, ROW_CLEAR D7
-#define ROW_CLOCK	4
-#define ROW_DATA	5
-#define ROW_LATCH	6
-#define ROW_OE  	-1
-#define ROW_CLEAR 	7
-
-#define COL_CLOCK 	8
-#define COL_DATA	9
-#define COL_LATCH	10
-#define COL_CLEAR	-1
-#define COL_OE		11
-
-#define NUM_ROW_DRIVERS 	1
-#define CHANNELS_PER_ROW 	24
-
-#define NUM_COL_DRIVERS 	1
-#define CHANNELS_PER_COL 	8
 
 #define ROWS	24
 #define COLS	8
